20558f813538d53b06043b08706fb7cca41b39f0
Jason Clark <jason_mc@btinternet.com>
2024-01-07 23:44:12 +0000

    first commit

---
+++ src/webparts/pnPGraph/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IPnPGraphWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'PnPGraphWebPartStrings' {
+  const strings: IPnPGraphWebPartStrings;
+  export = strings;
+}
